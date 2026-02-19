fbba4a21d198c60039b2028ab689763d3a51f429
Mandy Farrugia <mandy.farrugia.c10352@mcast.edu.mt>
2026-02-19 18:00:27 +0100

    Added reference to the Domain project for the DataAccess project.

---
+++ SWD62BSecurity2026/DataAccess/Context/TicketDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Context
+{
+    public class TicketDbContext : IdentityDbContext<IdentityUser>
+    {
+        public TicketDbContext(DbContextOptions<TicketDbContext> options) : base(options) {}
+
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+    }
+}
