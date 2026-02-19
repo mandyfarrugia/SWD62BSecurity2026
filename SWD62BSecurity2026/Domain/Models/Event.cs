5d4faa9523011b3e1b4738c51c2e71ba605ad46d
Mandy Farrugia <mandy.farrugia.c10352@mcast.edu.mt>
2026-02-19 17:55:23 +0100

    Defined the model classes with the appropriate annotations representing constraints.

---
+++ SWD62BSecurity2026/Domain/Models/Event.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Models
+{
+    public class Event
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public bool Public { get; set; }
+        public int MaximumTickets { get; set; }
+        public string FilePath { get; set; }
+        public string Organiser { get; set; }
+    }
+}
---
+++ SWD62BSecurity2026/Domain/Models/Ticket.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Models
+{
+    public class Ticket
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [ForeignKey(nameof(Event))]
+        public int EventFK { get; set; }
+        public virtual Event Event { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string IdCard { get; set; }
+        public Status Status { get; set; }
+    }
+}
