51b90a081b5a2ae9495ea3070d5090caad95cbf3
Mandy Farrugia <mandy.farrugia.c10352@mcast.edu.mt>
2026-04-15 07:30:21 +0200

    Defined the context class.

---
+++ MFSWD62B_AssimilationSolution/DataAccess/Context/TicketBookingSystemDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Context
+{
+    public class TicketBookingSystemDbContext : IdentityDbContext<IdentityUser>
+    {
+        public TicketBookingSystemDbContext(DbContextOptions<TicketBookingSystemDbContext> options) : base(options) {}
+
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseLazyLoadingProxies();
+        }
+    }
+}
